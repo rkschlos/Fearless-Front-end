2da65c21d9e01e15c3832f3a318f5bc4d4eed558
Rachael Schlosberg <rachael.schlosberg@gmail.com>
2022-04-26 16:38:19 -0500

    Added javascript that interacts with css in ghi/app.js and index.html to talk to api and display conference name, description, and image. Changed location list encoder properties to include picture_url.

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,4 +1,34 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', async () => {
 
-
-});
+    const url = 'http://localhost:8000/api/conferences/';
+  
+    try {
+      const response = await fetch(url);
+  
+      if (!response.ok) {
+        // Figure out what to do when the response is bad
+      } else {
+        const data = await response.json();
+  
+        const conference = data.conferences[0];
+        const nameTag = document.querySelector('.card-title');
+        nameTag.innerHTML = conference.name;
+  
+        const detailUrl = `http://localhost:8000${conference.href}`;
+        const detailResponse = await fetch(detailUrl);
+        if (detailResponse.ok) {
+          const details = await detailResponse.json();
+          const description = details.conference.description;
+          const descriptionTag = document.querySelector('.card-text');
+          descriptionTag.innerHTML = description;
+          const imageTag = document.querySelector('.card-img-top');
+          imageTag.src = details.conference.location.picture_url; //image src instead of innerHTML
+       
+        }
+  
+      }
+    } catch (e) {
+      // Figure out what to do if an error is raised
+    }
+  
+  });
