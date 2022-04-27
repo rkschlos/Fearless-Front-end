f1563781b2476c14fb5118ecd785023ed33f3a5f
Rachael Schlosberg <rachael.schlosberg@gmail.com>
2022-04-26 20:17:43 -0500

    Got through card template, got stuck on bootstrap.

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,34 +1,47 @@
+function createCard(name, description, pictureUrl) {
+    return `
+        <div class = "col"
+            <div class="card border-primary mb-3" style="padding: 2rem; box-shadow: 5px 5px lightblue;">
+                <img src= ${pictureUrl} class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">${name}</h5>
+                    <p class="card-text">${description}</p>
+                </div>
+            </div> 
+        </div>
+      `;
+  }
+
 window.addEventListener('DOMContentLoaded', async () => {
 
-    const url = 'http://localhost:8000/api/conferences/';
-  
-    try {
-      const response = await fetch(url);
-  
-      if (!response.ok) {
-        // Figure out what to do when the response is bad
-      } else {
-        const data = await response.json();
-  
-        const conference = data.conferences[0];
-        const nameTag = document.querySelector('.card-title');
-        nameTag.innerHTML = conference.name;
-  
+const url = 'http://localhost:8000/api/conferences/';
+
+try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+    // Figure out what to do when the response is bad
+    } else {
+    const data = await response.json();
+
+    for (let conference of data.conferences) {
         const detailUrl = `http://localhost:8000${conference.href}`;
         const detailResponse = await fetch(detailUrl);
         if (detailResponse.ok) {
-          const details = await detailResponse.json();
-          const description = details.conference.description;
-          const descriptionTag = document.querySelector('.card-text');
-          descriptionTag.innerHTML = description;
-          const imageTag = document.querySelector('.card-img-top');
-          imageTag.src = details.conference.location.picture_url; //image src instead of innerHTML
-       
+        const details = await detailResponse.json();
+        const name = details.conference.name;
+        const description = details.conference.description;
+        const pictureUrl = details.conference.location.picture_url;
+        const html = createCard(name, description, pictureUrl);
+        const column = document.querySelector('.row');
+        column.innerHTML += html;
+        console.log(html);
         }
-  
-      }
-    } catch (e) {
-      // Figure out what to do if an error is raised
     }
-  
-  });
+
+    }
+} catch (e) {
+    // Figure out what to do if an error is raised
+}
+
+});
